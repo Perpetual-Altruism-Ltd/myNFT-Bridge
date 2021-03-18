2f23943e7f35ea1330015a623536bd50e4241bca
Guillaume G <suricator@free.fr>
2021-03-18 16:44:18 +0100

    Proxyfication Stub

---
+++ Codebase/Solidity/contracts/ImplMemoryStructure.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity 0.8.2;
+
+/// @author Guillaume Gonnaud 2021
+/// @title ImplMemoryStructure
+/// @notice The well-ordered memory structure of our bridge. Used for generating proper memory address at compilation.
+contract ImplMemoryStructure {
+
+}
---
+++ Codebase/Solidity/contracts/ImplTransparentProxy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity 0.8.2;
+
+import "./ImplMemoryStructure.sol"
+
+/// @author Guillaume Gonnaud 2021
+/// @title ImplTransparentProxy
+/// @notice An ownable proxy transparent smart contract that is gonna be the memory space of the bridge.
+contract ImplTransparentProxy is ImplMemoryStructure /* is ERC-1538*/ {
+
+}
