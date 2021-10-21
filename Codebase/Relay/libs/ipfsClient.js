9e0239321992c7c30b3e87d4a67858cc21fdc042
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-10-21 14:05:34 +0200

    IPFS client

--- Codebase/Relay/libs/ipfsClient.js
+++ Codebase/Relay/libs/ipfsClient.js
@@ -1,16 +1,37 @@
-const Logger = require('./winston.js')('IPFS')
-const IPFS = require('ipfs-http-client')
+const Logger = require('./winston.js')('IPFS');
+const IPFS = require('ipfs-http-client');
 
 class IPFSClient {
-    constructor(){
-        this.ipfsInstance = IPFS.create('http://localhost:5001/api/v0')
+    constructor() {
+        this.ipfsInstance = IPFS.create('/ip4/127.0.0.1/tcp/5001')
     }
 
-    async addJson(json){
-        return await this.ipfsInstance.add(JSON.stringify(json))
+    /**
+     * Add JSON Object to IPFS
+     * @param {JSON Object} json 
+     */
+    async addJsonObj(json) {
+        return await this.ipfsInstance.add(JSON.stringify(json));
     }
 
-    async pinFile(cid){
-        return await this.ipfsInstance.pinFile(cid)
+    /**
+     * Add a file to IPFS
+     * @param {string} path : path of the file on IPFS (ex : '/tmp/myfile.jpg' will make the file available on {URL}{CID}/myfile.jpg path)
+     * @param {Uint8Array | Blob | String | Iterable<Uint8Array> | Iterable<number> | AsyncIterable<Uint8Array> | ReadableStream<Uint8Array>} content : file content
+     */
+    async addFileObj(path, content) {
+        return await this.ipfsInstance.add({ path, content });
+    }
+
+    /**
+     * Remove an object from IPFS
+     * @param {*} cid 
+     * Note : this function execute garbage collector on ipfs instance
+     */
+    async removeObj(cid) {
+        await this.ipfsInstance.pin.rm(cid);
+        return await this.ipfsInstance.repo.gc();
     }
 }
+
+module.exports = IPFSClient;
