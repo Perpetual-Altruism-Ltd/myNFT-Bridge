c67c52f5d6a09b8711ba46f741e3f4a9304eb51e
Alexandre Guillioud <alexandre@mynft.com>
2021-10-22 16:14:10 +0200

    WIP added premint functionalities

--- Codebase/Relay/libs/blockhainModules/ethereum.js
+++ Codebase/Relay/libs/blockhainModules/ethereum.js
@@ -1,7 +1,7 @@
 const Logger = require('../winston.js')('Ethereum')
 const Web3 = require('web3')
 const Conf = require('../../conf')
-const ERC721Abi = require('../../abis/erc721abi')
+const ERC721IOUAbi = require('../../abis/erc721IOU.json')
 const EventEmitter = require('events')
 
 class Ethereum extends EventEmitter {
@@ -9,23 +9,25 @@ class Ethereum extends EventEmitter {
         super()
         this.web3Provider = new Web3.providers.WebsocketProvider(rpc)
         this.web3Instance = new Web3(this.web3Provider)
+        this.web3Wallet = this.web3Instance.eth.accounts.wallet.add(Conf.relayPrivateKey)
+        this.web3Instance.eth.defaultAccount = this.web3Wallet.address
         
         Logger.info(`Web3 ethereum querier instanciated on rpc ${rpc}`)
     }
-    
-    listenForOperator(contract, tokenId) {
-        const web3Contract = new this.web3Instance.eth.Contract(ERC721Abi, contract)
 
-        web3Contract.once('Approval', { filter: { tokenId: tokenId } }, (err, data) => {
-            console.log('once', err, data)
-            this.emit('operatorSetted', data)
-        })
-    }
-
-    premintToken(universe, contract) {
-        return new Promise((resolve, reject) => {
-            resolve('123'.toString());
-        });
+    async premintToken(contractAddress) {
+        const networkId = await this.web3Instance.eth.net.getId();    
+        const contract = new this.web3Instance.eth.Contract(
+            ERC721IOUAbi,
+            contractAddress, 
+            { from: this.web3Instance.eth.defaultAccount, gas: 8000000 }
+        );
+        
+        const tx = await contract.methods.premintFor(this.web3Wallet.address).send();
+        const tokenId = await contract.methods.mintedTokens().call()
+        Logger.info(`Preminted a token ! Transaction hash : "${tx.transactionHash}". Token id "${tokenId}".`)
+        
+        return tokenId
     }
 }
 
