6c0cc367a6fea993e84c8b10d0ab5f05e84316f1
IRealIan <57064525+IRealIan@users.noreply.github.com>
2021-10-22 17:04:28 +0200

    update /initMigration and /pollingMigration

--- Codebase/Relay/libs/blockhainModules/ethereum.js
+++ Codebase/Relay/libs/blockhainModules/ethereum.js
@@ -1,18 +1,20 @@
 const Logger = require('../winston.js')('Ethereum')
 const Web3 = require('web3')
 const Conf = require('../../conf')
+// TODO : one require to import an ABIs object
 const ERC721Abi = require('../../abis/erc721abi')
+const BridgeAbi = {};
 const EventEmitter = require('events')
 
 class Ethereum extends EventEmitter {
-    constructor(rpc){
+    constructor(rpc) {
         super()
         this.web3Provider = new Web3.providers.WebsocketProvider(rpc)
         this.web3Instance = new Web3(this.web3Provider)
-        
+
         Logger.info(`Web3 ethereum querier instanciated on rpc ${rpc}`)
     }
-    
+
     listenForOperator(contract, tokenId) {
         const web3Contract = new this.web3Instance.eth.Contract(ERC721Abi, contract)
 
@@ -27,6 +29,71 @@ class Ethereum extends EventEmitter {
             resolve('123'.toString());
         });
     }
+
+    verifySignature(messageHash, signature) {
+        return this.web3Instance.eth.personal.ecRecover(messageHash, signature);
+    }
+
+    /* ==== Tokens ==== */
+    safeTransferFrom(contract, from, to, tokenId) {
+        return new Promise(async (resolve, reject) => {
+            const web3Contract = new this.web3Instance.eth.Contract(
+                ERC721Abi,
+                contract,
+                {
+                    from: this.web3Instance.eth.defaultAccount,
+                    gas: 8000000
+                }
+            );    
+            try {
+                const escrowHash = await web3Contract.methods.safeTransferFrom(from, to, tokenId).send();
+                resolve(escrowHash);
+            } catch(e) {
+                reject(e);
+            }
+        })
+    }
+
+    /* ==== Departure Bridge Interractions  ==== */
+    migrateToERC721IOU(migrationData, migrationSignature) {
+        return new Promise(async (resolve, reject) => {
+            // Check signee
+            const signee = await this.verifySignature(
+                this.web3Instance.utils.sha3(JSON.stringify(migrationData)),
+                migrationSignature
+            );
+
+            const web3Contract = new this.web3Instance.eth.Contract(
+                BridgeAbi,
+                migrationData.originWorld,
+                {
+                    from: this.web3Instance.eth.defaultAccount,
+                    gas: 8000000
+                }
+            );
+    
+            const data = [
+                migrationData.originWorld,
+                parseInt(migrationData.originTokenId),
+                migrationData.destinationUniverse, // eg : "Moonbeam"
+                migrationData.destinationBridge,
+                migrationData.destinationWorld,
+                migrationData.destinationTokenId,
+                migrationData.destinationOwner,
+                signee
+            ];
+    
+            try {
+                await web3Contract.methods.migrateToERC721IOU(...data).send();
+                resolve(true);
+            } catch(e) {
+                reject(e);
+            }
+        })
+    }
+
+    /* ==== Arrival Bridge Interractions  ==== */
+
 }
 
 module.exports = Ethereum
