dd643e3f1b91ff39e2506aa5a212eb420a62d142
Nico <nicolasf-n7@users.noreply.github.com>
2021-10-26 17:24:32 +0100

    initMigration call

--- Codebase/Bridge_Frontend/public/site/views/RegisterMigration.js
+++ Codebase/Bridge_Frontend/public/site/views/RegisterMigration.js
@@ -15,6 +15,34 @@ export default class extends AbstractView {
     let migData = model.migrationData;
     let account = window.web3.currentProvider.selectedAddress;
 
+    //Cookies management
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        } else {
+            var expires = "";
+        }
+        // document.cookie = name + "=" + value + expires + "; path=/; secure; samesite=strict";
+        document.cookie = name + "=" + value + expires + ";path=/; SameSite=Strict; Secure";
+    }
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ')
+                c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0)
+                return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
     //Ask user to grant the relay as an operator by calling approve from ERC721 contract
     let grantRelayOperatorPrivilege = async function(){
       try{
@@ -31,41 +59,47 @@ export default class extends AbstractView {
 
           let loadingText = document.getElementById("RegistrationLoadingText");
           if(loadingText != null && loadingText != undefined){loadingText.textContent = "Waiting for the relay to proceed to the token transfert.";}
-
-          signMigrationData();
         }).catch((res) => {
           console.log("Operator approval canceled or error");
           console.log(res);
 
           alert("Please approve the relay as an operator for this NFT. You must be to owner of that token.");
-          signMigrationData();// TODELETE, ONLY FOR TEST PURPOSE
         });
       }catch(err){
         console.log("Error when setting relay as an operator: " + err);
       }
     }
 
-    let signMigrationData = async function(){
-      /*try{
-        let selectedRelayIndex = getDropDownSelectedOptionIndex("RelaySelector");
-        let relayOgNetworkAddr = bridgeApp.relays[selectedRelayIndex].publicKey;
-        let originTokenId = migData.originTokenId;
+    //relay's backend interactions
+    let initMigration = async function(){
+      let selectedRelayIndex = migData.migrationRelayIndex;
+      let relayURL = bridgeApp.relays[selectedRelayIndex].url;
+      let destinationNetworkId = bridgeApp.networks[migData.destinationUniverseIndex].networkID.toString(16);
 
-        contracts.originalChainERC721Contract.methods.setApprove(relayOgNetworkAddr, originTokenId)
-        .send({from: account, gas: 30000})
-        .then((res) => {
-          console.log("Relay is now an operator");
-          document.getElementById("RegistrationLoadingText").textContent = "Waiting for the relay to proceed to the token transfert.";
-          //Waiting for the transaction to be accepted on the blockchain.
-        });
+      const xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          let migId = xhr.response;
+          //Add migrationID as cookie
+          createCookie("migrationId", migId, 31);
+          console.log("MigrationId: " + migId);
+        }
+      };
+      xhr.open('POST', relayURL + '/initMigration');
+      let requestParam = {};
+      requestParam.migrationData = {};
+      requestParam.migrationData.originUniverse = migData.originUniverse;
+      requestParam.migrationData.originWorld = migData.originWorld;
+      requestParam.migrationData.originTokenId = migData.originTokenId;
+      requestParam.migrationData.originOwner = "0x00";
+      requestParam.migrationData.destinationUniverse = migData.destinationUniverse;
+      requestParam.migrationData.destinationBridge = "0x00";
+      requestParam.migrationData.destinationWorld = migData.destinationWorld;
+      requestParam.migrationData.destinationTokenId = migData.destinationTokenId;
+      requestParam.migrationData.destinationOwner = migData.destinationOwner;
+      requestParam.operatorHash = "0x00";
 
-        console.log("Migration data signed");
-        document.getElementById("RegistrationLoadingText").textContent = "Waiting for the relay to proceed to the token transfert.";
-        //Waiting for the transaction to be accepted on the blockchain.
-      }catch(err){
-        console.log("Error when signing migration data: " + err);
-      }*/
-      console.log("Will sign...");
+      xhr.send(requestParam);
     }
 
     //Display migration data from model.migData
