46cbed1da2e6294dbbf22402117a11a0563ae4ea
Jcheng <jimmy.cheng.75013@gmail.com>
2022-02-01 14:55:09 +0100

    chore: add example of _localUniverse

--- Codebase/Bridge_Contracts/contracts/Implementation/BridgeFeatures/ImplMyNFTBridgeFunInit.sol
+++ Codebase/Bridge_Contracts/contracts/Implementation/BridgeFeatures/ImplMyNFTBridgeFunInit.sol
@@ -12,6 +12,13 @@ contract ImplMyNFTBridgeFunInit  is ImplMemoryStructure, IMyNFTBridgeFunInit {
     /// @notice Initialize a bridge
     /// @param _localUniverse The local universe this bridge is deployed in. Refer to ImplMemoryStructure for a list
     /// of standardized universes.
+    /// 0x07dac20e  //  RINKEBY
+    /// 0xee0bec75  // KOVAN
+    /// 0xe35d7d6b  // POLKADOT TESTNET MOONBASE ALPHA
+    /// 0x6d2f0e37 // Ethereum Mainnet
+    /// 0x06551a5b // Polkadot Moonriver
+    /// 0x51147ce2 // Polkadot Moonbeam
+    /// The hash (example : 0x51147ce2) is the 4 last bytes of the keccak256 hash of the universe name
     function init(
        uint256 _localUniverse
     ) external {
