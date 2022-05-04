d6c1036eb1418ab433cf1e0d935fd5b12bbe74f1
Alexandre Guillioud <alexandre@mynft.com>
2022-05-04 10:18:15 +0200

    Frontend now go through our API to fetch for IPFS ressources. Better speed since it go through S3 whenever possible.

--- Codebase/Bridge_Frontend/public/site/views/MigrationForm.js
+++ Codebase/Bridge_Frontend/public/site/views/MigrationForm.js
@@ -1297,7 +1297,7 @@ export default class extends AbstractView {
       const regexIpfs = /(Qm[1-9A-HJ-NP-Za-km-z]{44,}|b[A-Za-z2-7]{58,}|B[A-Z2-7]{58,}|z[1-9A-HJ-NP-Za-km-z]{48,}|F[0-9A-F]{50,})(\/.*)*/
       let res = tokenURI;
       if(regexIpfs.test(tokenURI)){
-          res = `https://ipfs.infura.io/ipfs/${tokenURI.match(regexIpfs)[0]}`
+          res = `https://api.mynft.com/ipfs/${tokenURI.match(regexIpfs)[0]}`
       }
       return res;
     }
