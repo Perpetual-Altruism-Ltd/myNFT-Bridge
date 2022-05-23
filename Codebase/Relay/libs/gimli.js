60313b33fc65f955521a5c754974b844488e27df
Alexandre Guillioud <alexandre@mynft.com>
2022-05-23 19:30:39 +0200

    Gimli class is now protected against errors

--- Codebase/Relay/libs/gimli.js
+++ Codebase/Relay/libs/gimli.js
@@ -2,45 +2,70 @@ const Logger = require('./winston.js')('Gimli')
 const Conf = require('../conf')
 const Axios = require('axios')
 const FormData = require('form-data')
+const Utils = require('./utils.js')
 
 class Gimli {
     constructor(){}
 
     async uploadFile(fileBuffer, name){
-        const formData = new FormData()
-        formData.append('file', fileBuffer, name)
+        try{
+            const formData = new FormData()
+            formData.append('file', fileBuffer, name)
+        
+            const result = await Axios.post(
+                `${Conf.gimliUrl}/api/files/uploadFile`
+                , formData
+                , { headers: formData.getHeaders() }
+            )
     
-        const result = await Axios.post(
-            `${Conf.gimliUrl}/api/files/uploadFile`
-            , formData
-            , { headers: formData.getHeaders() }
-        )
-
-        return result.data
+            return result.data
+        }catch(err){
+            console.log(err)
+            await Utils.sleep(2000)
+            return await this.uploadFile(fileBuffer, name)
+        }
     }
 
     async getFile(fileId){
-        const result = await Axios.get(`${Conf.gimliUrl}/api/files/${fileId}`)
+        try{
+            const result = await Axios.get(`${Conf.gimliUrl}/api/files/${fileId}`)
 
-        return result.data
+            return result.data
+        }catch(err){
+            console.log(err)
+            await Utils.sleep(2000)
+            return await this.getFile(fileId)
+        }        
     }
 
     async getS3Url(ipfsUrl){
-        const result = await Axios.post(
-            `${Conf.gimliUrl}/api/files/getUrl`
-            , { ipfsUrl }
-        )
+        try{
+            const result = await Axios.post(
+                `${Conf.gimliUrl}/api/files/getUrl`
+                , { ipfsUrl }
+            )
 
-        return result.data
+            return result.data
+        }catch(err){
+            console.log(err)
+            await Utils.sleep(2000)
+            return await this.getS3Url(ipfsUrl)
+        }                
     }
 
     async getIpfsUrl(s3Url){
-        const result = await Axios.post(
-            `${Conf.gimliUrl}/api/files/getUrl`
-            , { s3Url }
-        )
+        try{
+            const result = await Axios.post(
+                `${Conf.gimliUrl}/api/files/getUrl`
+                , { s3Url }
+            )
 
-        return result.data
+            return result.data
+        }catch(err){
+            console.log(err)
+            await Utils.sleep(2000)
+            return await this.getIpfsUrl(s3Url)
+        }   
     }
 }
 
